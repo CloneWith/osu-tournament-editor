15019d60f8a39ca451bc0813950f5226a1670749
CloneWith <clonewith@qq.com>
2025-03-02 18:04:30 +0800

    Add template component files

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QLocale>
+#include <QTranslator>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    // I18N initialisation
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "osu-tournament-editor_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            a.installTranslator(&translator);
+            break;
+        }
+    }
+
+    // Main window properties
+    MainWindow w;
+    w.setMinimumSize(640, 480);
+    w.show();
+
+    return a.exec();
+}
