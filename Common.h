b17e4a07130e2583ba7c15c31e4097aea34034ba
CloneWith <clonewith@qq.com>
2025-03-11 10:23:00 +0800

    Bump version for v0.1.0a

--- Common.h
+++ Common.h
@@ -12,7 +12,7 @@ namespace Common
     const QString PROJECT_NAME = "osu! Tournament Editor";
     const QString DATA_NAME = "osu-tournament-editor";
     const QString CONFIG_NAME = "editor-conf";
-    const QString VERSION = "0.0.1a";
+    const QString VERSION = "0.1.0a";
     const QList<QString> CREDITS = {"Copyright 2025 CloneWith"};
     const QString PROJECT_LINK = "https://github.com/CloneWith/osu-tournament-editor";
     const QString DOCUMENT_LINK = "https://github.com/CloneWith/osu-tournament-editor/wiki";
